e612f9f868a9ec88297eed29507266adc859a5dc
Moraesofia <sofiahj@hotmail.com>
2019-05-17 12:33:27 -0300

    Adicionada aba seguidores em perfil de ONG

--- Sistema/ivolunteer/src/app/perfil-ong/perfil-ong.component.ts
+++ Sistema/ivolunteer/src/app/perfil-ong/perfil-ong.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilOngComponent implements OnInit {
 
-  constructor() { }
+  seguidores: string[] = ['João Pedro','Natália Lopes','Nelson William', 'Sofia Moraes'];
+
+  constructor() { 
+
+    for (let index = 0; index < this.seguidores.length; index++) {
+      let seguidor = this.seguidores[index];
+      
+    }
+  }
 
   ngOnInit() {
   }
