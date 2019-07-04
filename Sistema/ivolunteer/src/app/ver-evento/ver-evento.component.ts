6da95111161c99357263c709e24d733069127cad
João Pedro Vieira <jp-a.v@hotmail.com>
2019-07-04 09:59:45 -0300

    Adicionada funcionalidade de confirmar interesse em evento

--- Sistema/ivolunteer/src/app/ver-evento/ver-evento.component.ts
+++ Sistema/ivolunteer/src/app/ver-evento/ver-evento.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService, EventsService, OngsService } from '../_services';
+import { AuthenticationService, EventsService, OngsService, VoluntariosService } from '../_services';
 import { ActivatedRoute, Router } from '@angular/router';
 import { first } from 'rxjs/operators';
-import { Event, Ong } from '../_models';
+import { Event, Ong, Usuario, Voluntario } from '../_models';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -21,13 +21,23 @@ export class VerEventoComponent implements OnInit {
   eventoDataTemp: string;
   eventoTimeTemp: string;
   public idEvento: number;
+  usuario: Usuario = new Usuario();
+  currentVoluntario: Voluntario = new Voluntario();
+  error: string = null;
+
+  // Ação a ser tomada ao se pressionar o botão "Confirmar interesse"
+  statusInterest: boolean = null;
+
+  // Texto que aparece junto ao botão de confirmar.
+  textInterest: string = null;
 
   constructor(public eventService: EventsService,
     public ongService: OngsService,
     public auth: AuthenticationService,
     public router: Router,
     private toastr: ToastrService,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private voluntarioService: VoluntariosService) {
 
     this.route.params.subscribe(params => {
       this.idEvento = params.id;
@@ -35,6 +45,9 @@ export class VerEventoComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.statusInterest = true;
+    this.textInterest = "Confirmar interesse";
+    this.loadCurrentVoluntario();
     this.patchEvento();
   }
 
@@ -55,4 +68,45 @@ export class VerEventoComponent implements OnInit {
       });
     });
   }
+
+  loadCurrentVoluntario() {
+    this.auth.currentUser.subscribe(data => {
+      this.usuario = data;
+    });
+    this.voluntarioService.getVoluntario(this.usuario.idVoluntario).subscribe(vol => {
+      this.currentVoluntario = vol;
+      this.currentVoluntario.idsEventosConfirmados.forEach(id => {
+        if (id == this.idEvento) {
+          this.statusInterest = false;
+          this.textInterest = 'Remover interesse no evento';
+        }
+      });
+    });
+  }
+
+  interest() {
+    const statusInterestString: string = String(this.statusInterest);
+    this.voluntarioService.subscribeOnEvent(this.idEvento, statusInterestString)
+      .pipe(first())
+      .subscribe(data => {
+        if (data) {
+          if (this.statusInterest) {
+            this.toastr.success('Você confirmou interesse neste evento');
+            this.statusInterest = false;
+            this.textInterest = 'Remover interesse no evento';
+          } else {
+            this.toastr.success('Você removeu seu interesse no evento');
+            this.statusInterest = true;
+            this.textInterest = 'Confirmar interesse';
+          }
+          this.patchEvento();
+          this.loadCurrentVoluntario();
+        }
+        error => {
+          this.error = JSON.stringify(error);
+          this.toastr.error(this.error);
+        };
+      });
+  }
+
 }
